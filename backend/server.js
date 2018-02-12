557df8396919937b03beea5f680ce27ad1496e83
Joshua Baker <joshuakb2@gmail.com>
2018-02-11 22:03:46 -0600

    I made changes to the backend and the front end.
    For consistency's sake, we should use the terms "participant" and "operator".
    
    When someone loads / or /operator, by default the send button is disabled.
    After connecting to the server with socket.io, each interface identifies itself as either "participant" or "operator".
    The server will then respond with a status update according to which roles were already filled.
    
    As soon as both the operator and participant are present, the participant's send button is enabled so they can ask a question.
    Once their question is asked, the send button is disabled again and the operator's send button is enabled.
    Once the operator has submitted their reply, the participant is given both responses and their send button is enabled again.
    
    Meanwhile, the description at the top of the page is being modified by the server at each step to reflect the current state
    of the turing test activity.

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,10 @@ var bot = new Cleverbot({
 });
 
 var bot_response = '';
-var human_response = '';
+var operator_response = '';
+
+var participant_socket = null;
+var operator_socket = null;
 
 // Server pages
 app.get('/', function(req, res){
@@ -25,28 +28,126 @@ app.get('/operator', function(req, res){
 
 // Events
 io.on('connection', function(socket){
-  console.log('user connected');
+  console.log('Unidentified user connected');
   socket.on('disconnect', function(){
-    console.log('user disconnected');
+    if(socket == participant_socket) {
+        console.log('Participant disconnected.');
+        if(operator_socket) {
+            operator_socket.emit('status', { ready: false, message: 'The participant has disconnected.' });
+        }
+
+        participant_socket = null;
+    }
+    else if(socket == operator_socket) {
+        console.log('Operator disconnected');
+        if(participant_socket) {
+            participant_socket.emit('status', { ready: false, message: 'The operator has disconnected.' });
+        }
+
+        operator_socket = null;
+    }
+    else {
+        console.log('Unidentified user disconnected');
+    }
   });
-  
-  socket.on('chat message', function(msg){
-		console.log('question: ' + msg);
-		io.emit('chat message', msg);
-		//bot_response = msg;
-		
-		bot.query(msg).then(function(res) {
-			console.log('Cleverbot: ' + res.output);
-			bot_response = res.output;
-			respond(bot_response, human_response);
-		});
-  });
+
+  //    Users must identify themselves according to their role in the activity
+  //    before we can move on.
+  socket.on('identify', function(role) {
+        switch(role) {
+            case 'participant':
+                //  If we don't have any participant yet
+                if(!participant_socket) {
+                    console.log('User identified as participant.');
+                    participant_socket = socket;
+
+                    participant_socket.on('message', onParticipantMessage);
+
+                    //  If we already have an operator
+                    if(operator_socket) {
+                        io.emit('reset');
+
+                        participant_socket.emit('status', { ready: true, message: 'The turing test has begun! Please send a message.' });
+                        operator_socket.emit('status', { ready: false, message: 'The turing test has begun! Waiting for the user to send a message.' });
+                    }
+                    //  Otherwise we're still waiting
+                    else {
+                        participant_socket.emit('status', { ready: false, message: 'You have connected to the turing test server! We\'re still waiting on our other human participant.' });
+                    }
+                }
+                //  If we already have a participant connected
+                else {
+                    //  Let them know and then drop this connection.
+                    socket.emit('status', { ready: false, message: 'There is already a participant connected to this server. Only 1 can be connected at a time.' }, function() {
+                        socket.disconnect(true);
+                    });
+
+                    return;
+                }
+
+                break;
+            case 'operator':
+                //  If we don't have any operators yet
+                if(!operator_socket) {
+                    console.log('User identified as operator.');
+                    operator_socket = socket;
+
+                    operator_socket.on('message', onOperatorMessage);
+
+                    //  If we already have a participant
+                    if(participant_socket) {
+                        io.emit('reset');
+
+                        participant_socket.emit('status', { ready: true, message: 'The turing test has begun! Please send a message.' });
+                        operator_socket.emit('status', { ready: false, message: 'The turing test has begun! Waiting for the user to send a message.' });
+                    }
+                    //  Otherwise we're still waiting
+                    else {
+                        operator_socket.emit('status', { ready: false, message: 'You have connected to the turing test server! We\'re still waiting on our other human participant.' });
+                    }
+                }
+                //  If we already have an operator connected
+                else {
+                    //  Let them know and then drop this connection.
+                    socket.emit('status', { ready: false, message: 'There is already an operator connected to this server. Only 1 can be connected at a time.' }, function() {
+                        socket.disconnect(true);
+                    });
+
+                    return;
+                }
+
+                break;
+            default:
+                //  This only occurs if something is coded incorrectly
+                console.log('User tried to identify as "' + role + '". Valid roles are "participant" and "operator".');
+
+                //  Let them know and then drop this connection.
+                socket.emit('status', { message: 'The role "' + role + '" is not understood.' }, function() {
+                    socket.disconnect(true);
+                });
+
+                return;
+        }
+/*
+        socket.on('chat message', function(msg){
+            console.log('question: ' + msg);
+            io.emit('chat message', msg);
+            //bot_response = msg;
+            
+            bot.query(msg).then(function(res) {
+                console.log('Cleverbot: ' + res.output);
+                bot_response = res.output;
+                respond();
+            });
+        });
 	
-	socket.on('human response', function(msg) {
-		console.log('human message: ' + msg);
-		human_response = msg;
-		respond(bot_response, human_response);
-	});
+        socket.on('human response', function(msg) {
+            console.log('human message: ' + msg);
+            operator_response = msg;
+            respond();
+        });
+*/
+  });
 });
 
 http.listen(PORT, function(){
@@ -54,10 +155,33 @@ http.listen(PORT, function(){
 });
 
 
-function respond(bot_response, human_response) {
-	if(bot_response != '' && human_response != '') {
-		io.emit('chat response', bot_response, human_response);
+function respond() {
+	if(bot_response != '' && operator_response != '') {
+		participant_socket.emit('message', { bot: bot_response, person: operator_response });
+        participant_socket.emit('status', { ready: true, message: 'Responses have arrived! Feel free to continue conversing.' });
 		bot_response = '';
-		human_response = '';
+		operator_response = '';
 	}
+}
+
+function onParticipantMessage(msg) {
+    bot.query(msg).then(function(res) {
+        console.log('Cleverbot: ' + res.output);
+        bot_response = res.output;
+        respond();
+    });
+
+    console.log('Participant: ' + msg);
+
+    participant_socket.emit('status', { ready: false, message: 'Waiting on responses from human and bot.' });
+    operator_socket.emit('message', { person: msg });
+    operator_socket.emit('status', { ready: true, message: 'Participant is waiting on your response.' });
+}
+
+function onOperatorMessage(msg) {
+    console.log('Operator: ' + msg);
+
+    operator_response = msg;
+    operator_socket.emit('status', { ready: false, message: 'Waiting on participant to send another message.' });
+    respond(); 
 }
